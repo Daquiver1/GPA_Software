5a4bdd81516e2e0b1527a99bac4fb3042d1ccdfe
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-01-19 22:52:54 +0000

    Added error handling

--- gui/logic1.py
+++ gui/logic1.py
@@ -8,6 +8,7 @@ Inputs: # number of courses, grades(A), credit hours of courses.
 Output: A grade
 """
 
+
 main_grades = {"A": 4.0, "B+": 3.5,
 		 "B": 3.0, "C+": 2.5, 
 		 "C": 2.0, "D+": 1.5,
@@ -17,15 +18,25 @@ main_grades = {"A": 4.0, "B+": 3.5,
 def gpa_calc():
 	count = 0
 	hours = 0
-	course_num = int(input("Please enter the num of courses you are offering: "))
+	try:
+		course_num = int(input("Please enter the num of courses you are offering: "))
+	except ValueError:
+		return "value must be an integer"
 	for i in range(course_num):
 		grades = input(f"Enter grade{i+1}: ").upper()
+
 		if grades not in main_grades:
 			return "Enter a valid grade"
-		c_hours = int(input(f"Enter the credit hour of grade {grades}: "))
+
+		try:
+			c_hours = int(input(f"Enter the credit hour of grade {grades}: "))
+		except ValueError:
+			return "Value must be an integer."
 			
 		count += main_grades[grades] * c_hours
 		hours += c_hours
-	gpa = (count) / (hours)
-	
-gpa_calc()
+
+	return count/hours
+
+
+print(gpa_calc())
