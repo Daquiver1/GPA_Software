8e5da2eedaa6cb4de8ec53c256142620b712e950
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-01-20 10:20:11 +0000

    Finished linking it with logic

--- gui/gpa_calc.py
+++ gui/gpa_calc.py
@@ -9,34 +9,40 @@ class InputDialog(QDialog):
 		super().__init__(parent)
 
 		self.course_num = self.getCourses()
+		self.grades = []
+		self.credits = []
 
 		buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self);
 		layout = QFormLayout(self)
 
-		self.grades = {}
 		for i in range(self.course_num):
-			self.grades[self.getText()] = self.getCredit()
+			self.grades.append(self.getText())
+			if self.grades[i] == None:
+				break
+			self.credits.append(self.getCredit())
 
-		showGPA(self)
+		self.showGPA()
 		  
 	def getCourses(self):
-		i, okPressed = QInputDialog.getInt(self, "Courses","Number of Courses:", 4, 0, 100, 1)
+		i, okPressed = QInputDialog.getInt(self, "Courses","Number of Courses:", 4, 0, 10, 1)
 		return i
 
 	def getText(self):
 		text, okPressed = QInputDialog.getText(self, "Grades","Enter your grade:", QLineEdit.Normal, "")
-		return text 
+		if len(text) == 1 or len(text) == 2:
+			return text.upper()
+		return None
 
 	def getCredit(self):
-		i, okpressed = QInputDialog.getInt(self, "Credit Hours", "Credit hour of grade:", 4, 0, 100, 1)
+		i, okpressed = QInputDialog.getInt(self, "Credit Hours", "Credit hour of grade:", 4, 0, 10, 1)
 		return i
 
 	def showGPA(self):
-		new_gpa_calc(self.grades)
+		yaw = new_gpa_calc(self.grades, self.credits)
+		QMessageBox.about(self, "Title", f"Your gpa is {yaw}")
 
 
 if __name__ == '__main__':
-
 	import sys
 	app = QApplication(sys.argv)
 	dialog = InputDialog()
