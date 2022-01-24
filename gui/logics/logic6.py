e0f4e57e3220aee4cbb505e4dde53a6b9c7d7342
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-01-24 22:09:50 +0000

    Added contact

---
+++ gui/logics/logic6.py
@@ -0,0 +1,13 @@
+def Myself():
+	yaw = f"""
+Hey, my name is Christian and I'm a student developer at the University of Ghana.
+I'm a solutions oriented individual, so if I see a problem or an inefficient solution, my brain automatically thinks of how it can be resolved.\n 
+You can contact me via:
+Gmail: Cabrokwa11@gmail.com
+Telegram: @Daquiver
+Github: https://github.com/Daquiver1
+Twitter: https://twitter.com/daquiver1"""
+	return yaw
+
+if __name__ == "__main__":
+	print(Myself())
