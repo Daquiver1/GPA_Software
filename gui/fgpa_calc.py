5ff4fd935fddc489dcf5d3a3d0bc006237902c19
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-01-25 10:38:45 +0000

    Interface for fgpa calc feature

---
+++ gui/fgpa_calc.py
@@ -0,0 +1,49 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from logics.logic5 import fgpa
+
+class InputDialog5(QDialog):
+	def __init__(self, parent=None):
+		super().__init__(parent)
+		self.random()
+
+
+	def random(self):
+		buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self);
+		layout = QFormLayout(self)
+		
+		self.first = self.getCGPA1()
+		self.second = self.getCGPA2()
+		self.third = self.getCGPA3()
+		self.fourth = self.getCGPA4()
+
+		self.showFGPA()
+		  
+	def getCGPA1(self):
+		i, okPressed = QInputDialog.getDouble(self, "Current","Level 100 CGPA: ", 0.00, 0, 4.0, 2)
+		return i
+
+	def getCGPA2(self):
+		i, okPressed = QInputDialog.getDouble(self, "Current","Level 200 CGPA: ", 0.00, 0, 4.0, 2)
+		return i
+
+	def getCGPA3(self):
+		i, okPressed = QInputDialog.getDouble(self, "Current","Level 300 CGPA: ", 0.00, 0, 4.0, 2)
+		return i
+
+	def getCGPA4(self):
+		i, okPressed = QInputDialog.getDouble(self, "Current","Level 400 CGPA: ", 0.00, 0, 4.0, 2)
+		return i
+
+	def showFGPA(self):
+		yaw = fgpa(self.first, self.second, self.third, self.fourth)
+		QMessageBox.about(self, "Title", yaw)
+
+if __name__ == '__main__':
+	import sys
+	app = QApplication(sys.argv)
+	dialog = InputDialog5()
+	exit(0)
+
