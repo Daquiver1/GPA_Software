1de80491e0167349f235405e7b56020027e6aeec
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-01-26 06:23:21 +0000

    Added interface for minimim CGPA

---
+++ gui/interfaces/min_cgpa.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.insert(0, "C:\\Users\\Anita Agyepong\\Documents\\Daquiver's Quivers\\Python\\GPA_Software\\gui\\logics")
+from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QFormLayout, QInputDialog, QMessageBox, QApplication
+from logic4 import *
+
+class InputDialog4(QDialog):
+	def __init__(self, parent=None):
+		super().__init__(parent)
+		self.main()
+
+
+	def main(self):
+		buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self);
+		layout = QFormLayout(self)
+
+		self.old_cgpa = self.getOldGpa()
+		self.old_cred = self.getOldCredit()
+		self.new_cred = self.getNewCredit()
+
+		self.showGPA()
+		  
+	def getOldGpa(self):
+		""" A function to retrieve the current CGPA of student. """
+		i, okPressed = QInputDialog.getDouble(self, "Current","Your Current GPA: ", 0.00, 0, 4.0, 2)
+		return i
+
+	def getOldCredit(self):
+		""" A function to retrieve the current credit hours of student. """
+		i, okpressed = QInputDialog.getInt(self, "Old Credit Hours", "Your current credit hours: ", 0, 0, 100, 1)
+		return i
+
+	def getNewCredit(self):
+		""" A function to retrieve the credit hours of a semester. """
+		i, okpressed = QInputDialog.getInt(self, "New Credit Hours", "Credit hours for this semester ", 0, 0, 30, 1)
+		return i
+
+	def showGPA(self):
+		""" A function to display the maximum CGPA of a student. """
+		min_cgpa = lowest_cgpa(self.old_cred, self.new_cred, self.old_cgpa)
+		return QMessageBox.about(self, "Title", min_cgpa)
+
+if __name__ == '__main__':
+	import sys
+	app = QApplication(sys.argv)
+	dialog = InputDialog4()
+	exit(0)
+
