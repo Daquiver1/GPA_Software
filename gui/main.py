f16c2079f689f3e2227347c50cc3f8cd52b1b76a
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-01-26 06:23:49 +0000

    Connected min_CGPA interface to main

--- gui/main.py
+++ gui/main.py
@@ -2,10 +2,11 @@ from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout, QInputDia
 from PyQt5.QtCore import Qt
 from interfaces.grades_needed import InputDialog2
 from interfaces.max_cgpa import InputDialog3
-from interfaces.gpa_calc import InputDialog4
-from interfaces.fgpa_calc import InputDialog5
-from interfaces.faqs import InputDialog6
-from interfaces.contact import InputDialog7
+from interfaces.min_cgpa import InputDialog4
+from interfaces.gpa_calc import InputDialog5
+from interfaces.fgpa_calc import InputDialog6
+from interfaces.faqs import InputDialog7
+from interfaces.contact import InputDialog8
 import sys
 
 class window(QWidget):
@@ -25,6 +26,7 @@ class window(QWidget):
       self.btn5 = QPushButton()
       self.btn6 = QPushButton()
       self.btn7 = QPushButton()
+      self.btn8 = QPushButton()
 
       self.l1.setText("Hey, Welcome To Daquiver's GPA Self Care")
       self.l2.setText("Below are the features we support")
@@ -35,14 +37,16 @@ class window(QWidget):
       self.btn2.clicked.connect(self.button2)
       self.btn3.setText("HIGHEST CGPA YOU CAN ATTAIN IN A SEM")
       self.btn3.clicked.connect(self.button3)
-      self.btn4.setText("GPA AND CGPA CALCULATOR")
+      self.btn4.setText("LOWEST CGPA YOU CAN ATTAIN IN A SEM")
       self.btn4.clicked.connect(self.button4)
-      self.btn5.setText("FGPA CALCULATOR")
+      self.btn5.setText("GPA AND CGPA CALCULATOR")
       self.btn5.clicked.connect(self.button5)
-      self.btn6.setText("FAQS")
+      self.btn6.setText("FGPA CALCULATOR")
       self.btn6.clicked.connect(self.button6)
-      self.btn7.setText("CONTACT")
+      self.btn7.setText("FAQS")
       self.btn7.clicked.connect(self.button7)
+      self.btn8.setText("CONTACT")
+      self.btn8.clicked.connect(self.button8)
 
       self.l1.setAlignment(Qt.AlignCenter)
       self.l2.setAlignment(Qt.AlignCenter)
@@ -60,6 +64,7 @@ class window(QWidget):
       self.vbox.addWidget(self.btn5)
       self.vbox.addWidget(self.btn6)
       self.vbox.addWidget(self.btn7)
+      self.vbox.addWidget(self.btn8)
 
       self.setLayout(self.vbox)
 
@@ -71,22 +76,26 @@ class window(QWidget):
       """ A function to connect btn3 with it's interface. """
       dlg = InputDialog3()
 
-   def button4(self,s):
+   def button4(self, s):
       """ A function to connect btn4 with it's interface. """
       dlg = InputDialog4()
 
-   def button5(self, s):
+   def button5(self,s):
       """ A function to connect btn5 with it's interface. """
       dlg = InputDialog5()
 
    def button6(self, s):
       """ A function to connect btn6 with it's interface. """
-      self.dlg = InputDialog6()           # When the method is left, the window will be destroyed. So I stored it by adding self.
-      self.dlg.show()
+      dlg = InputDialog6()
 
    def button7(self, s):
       """ A function to connect btn7 with it's interface. """
-      dlg = InputDialog7()
+      self.dlg = InputDialog7()           # When the method is left, the window will be destroyed. So I stored it by adding self.
+      self.dlg.show()
+
+   def button8(self, s):
+      """ A function to connect btn8 with it's interface. """
+      dlg = InputDialog8()
 
 
 if __name__ == '__main__':
